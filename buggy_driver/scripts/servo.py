fee1388ad32850042cc631edaeb73655e280d14c
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2015-09-28 14:48:06 +0200

    [Driver]Add motor PPM code

--- buggy_driver/scripts/servo.py
+++ buggy_driver/scripts/servo.py
@@ -5,34 +5,45 @@ from std_msgs.msg import Int16
 from geometry_msgs.msg import Twist
 import Adafruit_BBIO.PWM as PWM
 
+# Servo PPM configuration
 servo_pin = "P8_13"
-duty_min = 6.6
-duty_max = 12
-duty_span = duty_max - duty_min
+duty_servo_min = 6.6
+duty_servo_max = 12
+duty_servo_span = duty_servo_max - duty_servo_min
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-    angle = data.angular.z
-    if angle == 42:
-        PWM.stop(servo_pin)
-        PWM.cleanup()
-        return
-    angle_f = float(angle)
-    duty = ((-angle_f/2 + 0.5) * duty_span + duty_min)
+# Motor PPM configuration
+motor_pin = "P8_14"
+duty_motor_min = 6
+duty_motor_max = 12
+duty_motor_span = duty_motor_max - duty_motor_min
+
+def setServoAngle(angle_f):
+    duty = ((-angle_f/2 + 0.5) * duty_servo_span + duty_servo_min)
     PWM.set_duty_cycle(servo_pin, duty)
 
-def servo():
+def setMotorSpeed(speed_f):
+    duty = ((-speed_f/2 + 0.5) * duty_motor_span + duty_motor_min)
+    PWM.set_duty_cycle(motor_pin, duty)
+    
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+    angle_f = float(data.angular.z)
+    speed_f = float(data.linear.x)
+    setServoAngle(angle_f)
+    setMotorSpeed(speed_f)
 
-    rospy.init_node('servo', anonymous=True)
+def driver():
+    rospy.init_node('driver', anonymous=True)
     rospy.Subscriber("cmd_vel", Twist, callback)
     
-    PWM.start(servo_pin, duty_span * 0.5 + duty_min, 60.0)
+    PWM.start(servo_pin, duty_servo_span * 0.5 + duty_servo_min, 60.0)
+    PWM.start(motor_pin, duty_motor_span * 0.5 + duty_motor_min, 60.0)
 
     rospy.spin()
 
 
 if __name__ == '__main__':
     try:
-        servo()
+        driver()
     except rospy.ROSInterruptException:
         pass
